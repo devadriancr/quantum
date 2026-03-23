754817d52bad19ff9062552b296d8782d86d00ce
Adrian Camacho <adriancamacho18121997@gmail.com>
2026-03-23 11:06:23 -0600

    chore: administrator credentials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        \App\Models\User::create([
+            'name' => 'Usuario Administrador',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
 
         $this->call([
             DashboardTableSeeder::class,
